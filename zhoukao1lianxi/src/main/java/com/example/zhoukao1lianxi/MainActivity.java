cc2a091314828965524ebf2d6f3d48297b0c5877
xietianen <2457798068@qq.com>
2019-12-30 14:41:00 +0800

    第2次上传

---
+++ zhoukao1lianxi/src/main/java/com/example/zhoukao1lianxi/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.zhoukao1lianxi;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zhoukao1lianxi.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    private android.support.v4.view.ViewPager vp;
+    private android.support.design.widget.TabLayout tab;
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+        vp = (ViewPager) findViewById(R.id.vp);
+        tab = (TabLayout) findViewById(R.id.tab);
+    }
+
+    @Override
+    protected void initListener() {
+
+        MyVp myVp=new MyVp(getSupportFragmentManager());
+        vp.setAdapter(myVp);
+        tab.setupWithViewPager(vp);
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
